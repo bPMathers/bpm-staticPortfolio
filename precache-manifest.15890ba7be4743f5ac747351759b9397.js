6e31845abe4fcf42d8dc900f36c1db7d45f9184f
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-07-03 12:52:44 -0400

    Updates

---
+++ precache-manifest.15890ba7be4743f5ac747351759b9397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e94d488d89155e7ce59012a76f4dc2",
+    "url": "/bpm-staticPortfolio/index.html"
+  },
+  {
+    "revision": "83bfce85fa48dcae1354",
+    "url": "/bpm-staticPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c783b4595c0e01c24d54",
+    "url": "/bpm-staticPortfolio/static/js/2.6e6d3f95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bpm-staticPortfolio/static/js/2.6e6d3f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bfce85fa48dcae1354",
+    "url": "/bpm-staticPortfolio/static/js/main.4c6d27bf.chunk.js"
+  },
+  {
+    "revision": "829a7739c22589b419db",
+    "url": "/bpm-staticPortfolio/static/js/runtime-main.eea7fe23.js"
+  }
+]);
